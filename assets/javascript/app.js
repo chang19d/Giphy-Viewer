868f9203d08ca97281f38cea2d900fd8364b9e64
DChang <chang19d@gmail.com>
2018-09-06 16:24:12 -0400

    added basic html with no styles

---
+++ assets/javascript/app.js
@@ -0,0 +1,2 @@
+var animalsArray = ["bird", "cat", "dog", "cow", "goat", "sugar glider", "hedgehog", "fennec fox", "kinkajou", "wallaby",
+                    "capybara", "slow loris", "dik dik", "binturong", "bearded dragon", "axolotl", "alpaca", "llama"];
